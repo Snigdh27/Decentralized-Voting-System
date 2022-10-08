e640bc5adab62aca0782f72f594d6fac3f5b3af8
Snigdh Kulshreshtha <74950528+Snigdh27@users.noreply.github.com>
2022-10-09 02:08:03 +0530

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+// import AddCandidate from './components/AddCandidate';
+import RemoveCandidate from './components/RemoveCandidate';
+// import CandidateDetails from './components/CandidateDetails';
+
+// import ElectionDetails from './components/ElectionDetails';
+// import CreateElection from './components/CreateElection';
+// import AdminDashboard from './components/AdminDashboard';
+// import ElectionResults from './components/ElectionResults';
+
+// import WinningPrediction from './components/WinningPrediction';
+
+// import UserVoting from './components/UserVoting';
+// import UserElections from './components/UserElections';
+// import UserRegisterForm from './components/UserRegisterForm';
+// import CoverPage from './components/CoverPage';
+// import AdminLogin from './components/AdminLogin';
+// import UserLogin from './components/UserLogin';
+// import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+// import UserDashboard from './components/UserDashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router>
+        <Link to="">CoverPage</Link>
+        <Link to="/user-login">User Login</Link>
+        <Route path='' component={CoverPage}/>
+        <Route path='/user-login' component={UserLogin}/>
+      </Router> */}
+      {/* <CoverPage/> */}
+      {/* <UserLogin/> */}
+      <RemoveCandidate/>
+      {/* <UserDashboard/> */}
+      {/* <UserRegisterForm/> */}
+      {/* <UserElections/> */}
+      {/* <UserVoting/> */}
+      {/* <ElectionResults/> */}
+    </>
   );
 }
 
