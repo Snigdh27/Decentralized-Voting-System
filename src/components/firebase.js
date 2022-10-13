20aaa7a011cb8aacc9bdb07956224ade401af6ae
Harshul14 <71930077+Harshul14@users.noreply.github.com>
2022-10-13 14:31:23 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,13 +2,21 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5yPmaVgw3spqJwTV9vR1Cp3o9e2samBY",
-  authDomain: "react-authentication-be710.firebaseapp.com",
-  projectId: "react-authentication-be710",
-  storageBucket: "react-authentication-be710.appspot.com",
-  messagingSenderId: "230481302273",
-  appId: "1:230481302273:web:f133ce36c9239669cf8100",
-  measurementId: "G-2MV57J0X4N"
+//  apiKey: "AIzaSyA5yPmaVgw3spqJwTV9vR1Cp3o9e2samBY",
+//  authDomain: "react-authentication-be710.firebaseapp.com",
+//  projectId: "react-authentication-be710",
+//  storageBucket: "react-authentication-be710.appspot.com",
+//  messagingSenderId: "230481302273",
+//  appId: "1:230481302273:web:f133ce36c9239669cf8100",
+//  measurementId: "G-2MV57J0X4N"
+  
+  apiKey: "AIzaSyA9QC0EMc0lZj4MfSbOWAHUi8jo27K-qj8",
+  authDomain: "react-minor-demo.firebaseapp.com",
+  databaseURL: "https://react-minor-demo-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "react-minor-demo",
+  storageBucket: "react-minor-demo.appspot.com",
+  messagingSenderId: "56042135778",
+  appId: "1:56042135778:web:5564362569ca2786573ebb"
 };
 
 // Initialize Firebase
