da44acfe25b27258ee5503b31c7a4d3fa3ee0d73
Snigdh27 <snigdh27@gmail.com>
2022-11-18 00:49:33 +0530

    "changes updated"

--- src/components/RemoveCandidate.js
+++ src/components/RemoveCandidate.js
@@ -1,47 +1,74 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react'
 import '../css/removeCandidate.css';
+import '../css/voting.css';
+import Remove from './Remove';
 import Sidebar from './Sidebar';
+import swal from 'sweetalert';
+import { Link } from 'react-router-dom';
+import fireDb from "./firebase";
+import {toast,ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function RemoveCandidate() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+      fireDb.child("voting_system").on("value", (snapshot) => {
+          if (snapshot.val() !== null) {
+              setData({ ...snapshot.val() });
+          } else {
+              setData({});
+          }
+
+      });
+
+      return () => {
+          setData({});
+      };
+  }, []);
+
+const successMessage=async(e)=>{
+  e.preventDefault();
+  swal({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success",
+    button: "Aww yiss!",
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("Registered Successfully for Voting!!!", {
+        icon: "success",
+      });
+    } else {
+      swal("Your data is saved!");
+    }
+  });
+}
+
+
+
   return (
     <>
   
   <Sidebar title="Admin"/>
-  <div className="remove-candidate">
-  <div className="container-registration">
-    <div className="title">REMOVE CANDIDATE</div>
-    <br />
-    <div className="content">
-      <form action="#">
-        <div className="user-details">
-          <div className="input-box">
-            <span className="details">Name</span>
-            <input
-              type="text"
-              placeholder="Enter candidate's name"
-              required=""
-            />
-          </div>
-          <div className="input-box">
-            <span className="details">Party</span>
-            <input
-              type="text"
-              placeholder="Enter political party"
-              required=""
-            />
-          </div>
-          <div className="input-box">
-            <span className="details">Date of Birth</span>
-            <input type="date" placeholder="" required="" />
-          </div>
-        </div>
-        <div className="button">
-          <input type="submit" defaultValue="Remove Candidate" value="Remove Candidate"/>
-        </div>
-      </form>
-    </div>
+  <div className="user-voting">
+  <h1>Remove Candidate</h1>
+  <section className="profile-cards">
+  {
+    Object.keys(data).map((id,index)=>{
+      return(
+        <Remove candidateName={data[id].name} imgUrl={data[id].url} party={data[id].party} experience={data[id].experience} age={data[id].age} state={data[id].region} district={data[id].district} uid={id} >
+          
+          </Remove>
+        
+      )
+    })
+  }
+  </section>
   </div>
-</div>
+  <ToastContainer/>
 </>
 
   )
