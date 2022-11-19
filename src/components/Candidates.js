83c908e16c4b3b8e24892a8e70d8358fd4b1542b
Snigdh27 <snigdh27@gmail.com>
2022-11-19 23:34:31 +0530

    "changes updated"

--- src/components/Candidates.js
+++ src/components/Candidates.js
@@ -1,9 +1,85 @@
 import React from 'react';
 import '../css/voting.css';
-
+import jsPDF from 'jspdf';
+import {render} from "react-dom"
 import { useState } from "react";
 import { ethers } from "ethers";
 import { Link } from 'react-router-dom';
+import {renderToString} from "react-dom/server";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// const styles = {
+//   fontFamily: "sans-serif",
+//   textAlign: "center"
+// };
+// const colstyle = {
+//   width: "30%"
+// };
+// const tableStyle = {
+//   width: "100%"
+// };
+// const Prints = () => (
+//   <div>
+
+//     <h3>Time & Materials Statement of Work (SOW)</h3>
+    
+//     <h4>General Information</h4>
+//     <table id="tab_customers" class="table table-striped" style={tableStyle}>
+//       <colgroup>
+//         <col span="1" style={colstyle} />
+//         <col span="1" style={colstyle} />
+//       </colgroup>
+//       <thead>
+//         <tr class="warning">
+//           <th>SOW Creation Date</th>
+//           <th>SOW Start Date</th>
+//           <th>Project</th>
+//           <th>Last Updated</th>
+//           <th>SOW End Date</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           <td>Dec 13, 2017</td>
+//           <td>Jan 1, 2018</td>
+//           <td>NM Connect - NMETNMCM</td>
+//           <td>Dec 13, 2017</td>
+//           <td>Dec 31, 2018</td>
+//         </tr>
+//       </tbody>
+//     </table>
+//     <p>
+//       This is a Time and Materials Statement of Work between Northwestern Mutual
+//       Life Insurance Company and Infosys with all general terms and conditions
+//       as described in the current Master Agreement and its related documents
+//     </p>
+//   </div>
+// );
+
+// const print = () => {
+//   const string = renderToString(<Prints />);
+//   const pdf = new jsPDF("p", "mm", "a4");
+//   const columns = [
+//     "SOW Creation Date",
+//     "SOW Start Date",
+//     "Project",
+//     "Last Updated",
+//     "SOW End Date"
+//   ];
+//   var rows = [
+//     [
+//       "Dec 13, 2017",
+//       "Jan 1, 2018",
+//       "ABC Connect - ABCXYZ",
+//       "Dec 13, 2017",
+//       "Dec 31, 2018"
+//     ]
+//   ];
+//   pdf.fromHTML(string);
+//   pdf.save("pdf");
+// };
+
 
 const startPayment = async ({ setError, setTxs, ether, addr }) => {
   try {
@@ -19,6 +95,8 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
       value: ethers.utils.parseEther(ether)
     });
     console.log({ ether, addr });
+    toast.success("Transaction Successful");
+    toast("Click on Upvote for final step")
     console.log("tx", tx);
     setTxs([tx]);
   } catch (err) {
@@ -27,6 +105,94 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
 };
 
 function Candidates(props) {
+
+  const styles = {
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    
+  };
+  const colstyle = {
+    width: "30%"
+  };
+  const tableStyle = {
+    width: "100%"
+  };
+  const Prints = () => (
+    <div>
+  
+      <h1>#-----Decentralized Voting System-----#</h1>
+      <br/>
+      <br/>
+      <h2>Voting Receipt</h2>
+
+      <h3>------------------------------------------------------------------------------------------</h3>
+      <br/>
+      {/* <table id="tab_customers" class="table table-striped" style={tableStyle}>
+        <colgroup>
+          <col span="1" style={colstyle} />
+          <col span="1" style={colstyle} />
+        </colgroup>
+        <thead>
+          <tr class="warning">
+            <th>SOW Creation Date</th>
+            <th>SOW Start Date</th>
+            <th>Project</th>
+            <th>Last Updated</th>
+            <th>SOW End Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props.candidateName}</td>
+            <td>Jan 1, 2018</td>
+            <td>NM Connect - NMETNMCM</td>
+            <td>Dec 13, 2017</td>
+            <td>Dec 31, 2018</td>
+          </tr>
+        </tbody>
+      </table> */}
+      <h3>Candidate : {props.candidateName}</h3>
+      <h3>Party : {props.party}</h3>
+      <h3>District : {props.district}</h3>
+      <h3>State : {props.state}</h3>
+      <br/>
+      <h3>------------------------------------------------------------------------------------------</h3>
+
+      <br/>
+      <h1> # Responsible Voter # </h1>
+      <br/>
+      <h4>
+        
+          <p>"Voting is not just our right but our duty as well", this saying goes a long way<br/> as it tells us clearly we have responsibilities as citizens of the country. <br/>To make sure we citizens run and take good care of our country we must put in our votes</p>
+          <p>A lot of time people choose to believe that single vote won't make much of a difference <br/>but that is far from the truth and people must realize it as soon as possible.</p>
+       
+      </h4>
+
+    </div>
+  );
+  
+  const print = () => {
+    const string = renderToString(<Prints />);
+    const pdf = new jsPDF("p", "mm", "a4");
+    const columns = [
+      "SOW Creation Date",
+      "SOW Start Date",
+      "Project",
+      "Last Updated",
+      "SOW End Date"
+    ];
+    var rows = [
+      [
+        "Dec 13, 2017",
+        "Jan 1, 2018",
+        "ABC Connect - ABCXYZ",
+        "Dec 13, 2017",
+        "Dec 31, 2018"
+      ]
+    ];
+    pdf.fromHTML(string);
+    pdf.save("Voting Receipt");
+  };
 
   const [error, setError] = useState();
   const [txs, setTxs] = useState([]);
@@ -39,7 +205,7 @@ function Candidates(props) {
       setError,
       setTxs,
       ether: "0.0000001",
-      addr: "0x45643d4C54775597D9D8FA91a546183308CDEF22"
+      addr: "0xed75306F1C7d3c9a79463689dBBB8155BA9dF43E"
     });
   };
 
@@ -62,15 +228,17 @@ function Candidates(props) {
         <div className="stats">
         <form onSubmit={handleSubmit}>
         <input className="stat upvoting" style={{color:"white",backgroundColor:"#161623"}} type="submit" value="Vote"/>
+        &nbsp;&nbsp;&nbsp;
+        <button className="stat upvoting" style={{color:"white",backgroundColor:"#161623"}} onClick={print} ><i className='bx bxs-upvote icon ' /></button>
         
-        <Link to="/user/elections"> Upvote</Link>
-        <i className='bx bxs-upvote icon'/>
           {/* </button> */}
           </form>
         </div>
       </div>
 
     </div>
+
+    
 
     <div className="profile-card-2 profile-card-1">
     <div className="name" style={{marginTop:"50px",fontSize:"1.40rem"}}>{props.candidateName}</div>
@@ -88,8 +256,9 @@ function Candidates(props) {
             <li>District : {props.district}</li>
           </ul>
         </div>
+        <ToastContainer />
     </div>
-
+    <button className="stats stat upvoting" style={{color:"black"}} onClick={print}>Download Receipt</button>
     </>
   )
 }
